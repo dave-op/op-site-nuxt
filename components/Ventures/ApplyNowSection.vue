6586108480261ecc417df2d227a01fc62539849a
SeniorEng <ademiagood@gmail.com>
2022-12-12 10:48:22 -0800

    Complete OP Ventures Page

---
+++ components/Ventures/ApplyNowSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-40 text-center text-black flex flex-col items-center">
+    <div class="uppercase italic text-[#ff8f8f] text-7xl mb-5">Apply Now</div>
+    <div class="text-2xl mb-10 w-5/12">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+      viverra at quam in vestibulum.
+    </div>
+    <div class="w-1/2 flex flex-col gap-5 mb-10">
+      <input placeholder="Full Name" class="px-8 py-3 bg-gray-200" />
+      <input placeholder="Company Name?" class="px-8 py-3 bg-gray-200" />
+      <input placeholder="Email Address?" class="px-8 py-3 bg-gray-200" />
+      <input placeholder="Phone Number" class="px-8 py-3 bg-gray-200" />
+      <textarea placeholder="Message" rows="7" class="px-8 py-3 bg-gray-200"></textarea>
+    </div>
+    <Button>Find out more</Button>
+  </div>
+</template>
---
+++ components/Ventures/WhatWeDoSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="p-40 bg-purple-1 text-white text-center flex justify-center">
+    <div class="w-1/2 flex flex-col gap-20">
+      <div class="uppercase italic text-6xl text-[#ff8f8f]">
+        Help Startups get off the ground
+      </div>
+      <div class="uppercase italic text-6xl text-[#ff8f8f]">
+        Develop digital products for part
+      </div>
+      <div class="uppercase italic text-6xl text-[#ff8f8f]">
+        Dedicated strategy, technical, and design
+      </div>
+      <div v-for="(data, index) in DATA" :key="index">
+        <div class="text-10xl uppercase italic">
+          {{ (index + 1).toLocaleString("en-US", { minimumIntegerDigits: 2 }) }}
+        </div>
+        <div class="text-6xl uppercase italic mb-10">{{ data.name }}</div>
+        <div class="text-2xl">{{ data.description }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const DATA = [
+  {
+    name: "A technical partner for your startup",
+    description:
+      "We’ll work with you to assist in defining your digital product strategy, starting by learning about your business goals, product value proposition, current and desired market positioning, along with the value that you want to convey to your customers. We complete a detailed, data-driven and research-based perspective to this process through testing, validating, iterating and then iterating again.",
+  },
+  {
+    name: "Align incentives, succeed together",
+    description:
+      "First impressions are key as users will make their minds up within 3 seconds. Strong design enables you to engage meaningfully with your users. Our team of creative designers work alongside you during each step of the design process from brand identity, rapid prototyping, MVP’s through to detailed high-fidelity UX and UI designs. We utilise design best practices, data, research, consumer psychology and human-centred design to provide beautiful and engaging products",
+  },
+  {
+    name: "A proven track record of startup success",
+    description:
+      "Getting a venture off the ground isn’t easy. Trust us – we’ve done it many times before. Knowing what success and failure looks like means we can help you navigate the process, reduce the risk of failure and find product-market fit faster.",
+  },
+];
+</script>
